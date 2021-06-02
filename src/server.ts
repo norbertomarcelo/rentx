867698f0e50ba34b78c129e049a9b78b25f06955
Marcelo Norberto <norbertomarcelo8@gmail.com>
2021-06-02 05:18:31 -0300

    Conhecendo o Docker

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
