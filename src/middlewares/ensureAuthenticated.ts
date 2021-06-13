9a4ba30911dc158867473df70115c865f0e1eb8a
Marcelo Norberto <norbertomarcelo8@gmail.com>
2021-06-13 19:28:02 -0300

    Avatar do Usuário

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -33,8 +33,9 @@ export async function ensureAuthenticated(
 
     if (!user) {
       throw new AppError("User does not exists!", 401);
-      
     }
+
+    request.user = { id: user_id };
 
     next();
   } catch {
