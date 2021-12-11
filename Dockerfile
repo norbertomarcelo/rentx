199f7fdf83e194fb50906ada2a63a99cfe1c1da3
Marcelo Norberto <norbertomarcelo8@gmail.com>
2021-12-11 19:16:41 -0300

    Requisitos

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node
+FROM node:14.18.2
 
 WORKDIR /usr/app
 
--- src/modules/cars/infra/typeorm/entities/Category.ts
+++ src/modules/cars/infra/typeorm/entities/Category.ts
@@ -3,7 +3,6 @@ import { v4 as uuidV4 } from "uuid";
 
 @Entity("categories")
 class Category {
-  
   @PrimaryColumn()
   id?: string;
 
